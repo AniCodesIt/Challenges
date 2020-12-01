90a0a976afa17da94515eb81f9fc64c448954529
AniCodesIt <73564845+AniCodesIt@users.noreply.github.com>
2020-11-30 22:11:06 -0600

    Add files via upload

---
+++ Greeter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Challenges
+{
+    class Greeter
+    {
+        public void Hello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+        public void Goodbye(string name)
+        {
+            Console.WriteLine("Goodbye " + name);
+        }
+
+        public void Time()
+        {
+
+            int hour = DateTime.Now.Hour;
+
+
+            if (hour >= 1 && hour <= 12)
+            {
+                Console.WriteLine("Good morning");
+            }
+            if (hour >= 13 && hour <= 18)
+            {
+                Console.WriteLine("Good afternoon");
+            }
+            if (hour >= 19 && hour <= 20)
+            {
+                Console.WriteLine("Good evening");
+            }
+            if (hour >= 21)
+            {
+                Console.WriteLine("Good night");
+            }
+
+
+
+
+
+        }
+    }
+    
+
+}
+
+
+    
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Challenges
+{
+    class GreeterUI
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Greeter _greeterWindow = new Greeter();
+            Console.WriteLine("Please tell me your name");       
+            name = Console.ReadLine();
+            _greeterWindow.Hello(name);
+            _greeterWindow.Goodbye(name);
+            _greeterWindow.Time();
+            Console.ReadLine();
+        }
+    }
+}
